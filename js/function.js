5f0e905a3a0e4019419454abee884265b67084e0
Claudio Meseri <cgmeseri@gmail.com>
2023-02-27 13:23:36 -0300

    CV - Claudio Meseri

---
+++ js/function.js
@@ -0,0 +1,74 @@
+/** alert("Curriculum vitae de Claudio Gabrel Meseri"); */
+
+document.getElementById("boton_mostrar").addEventListener('click', function() {
+    document.getElementById("ocultar").style.display = "block";
+});
+
+document.getElementById("boton_mostrar").addEventListener('click', function() {
+    document.getElementById("boton_mostrar").style.display = "none";
+});
+
+document.getElementById("boton_ocultar").addEventListener('click', function() {
+    document.getElementById("ocultar").style.display = "none";
+});
+
+document.getElementById("boton_ocultar").addEventListener('click', function() {
+    document.getElementById("boton_mostrar").style.display = "block";
+});
+
+
+
+document.getElementById("boton_mostrar1").addEventListener('click', function() {
+    document.getElementById("ocultar1").style.display = "block";
+});
+
+document.getElementById("boton_mostrar1").addEventListener('click', function() {
+    document.getElementById("boton_mostrar1").style.display = "none";
+});
+
+document.getElementById("boton_ocultar1").addEventListener('click', function() {
+    document.getElementById("ocultar1").style.display = "none";
+});
+
+document.getElementById("boton_ocultar1").addEventListener('click', function() {
+    document.getElementById("boton_mostrar1").style.display = "block";
+});
+
+
+
+document.getElementById("boton_mostrar2").addEventListener('click', function() {
+    document.getElementById("ocultar2").style.display = "block";
+});
+
+document.getElementById("boton_mostrar2").addEventListener('click', function() {
+    document.getElementById("boton_mostrar2").style.display = "none";
+});
+
+document.getElementById("boton_ocultar2").addEventListener('click', function() {
+    document.getElementById("ocultar2").style.display = "none";
+});
+
+document.getElementById("boton_ocultar2").addEventListener('click', function() {
+    document.getElementById("boton_mostrar2").style.display = "block";
+});
+
+
+
+document.getElementById("boton_mostrar3").addEventListener('click', function() {
+    document.getElementById("ocultar3").style.display = "block";
+});
+
+document.getElementById("boton_mostrar3").addEventListener('click', function() {
+    document.getElementById("boton_mostrar3").style.display = "none";
+});
+
+document.getElementById("boton_ocultar3").addEventListener('click', function() {
+    document.getElementById("ocultar3").style.display = "none";
+});
+
+document.getElementById("boton_ocultar3").addEventListener('click', function() {
+    document.getElementById("boton_mostrar3").style.display = "block";
+});
+
+
+
